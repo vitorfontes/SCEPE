07e2e00fe5808870171d92970a309880cd9f60ce
Leonardo <leonardo_oliveira1@outlook.com>
2019-10-25 13:02:16 -0300

    Ajuste do menu

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule
   ],
   exports: [LoginComponent]
 })
